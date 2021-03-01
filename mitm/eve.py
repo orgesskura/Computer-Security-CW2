7c469cffba4a10e8a8752ec61c6de67adc96429d
orgesskura <s1813106@ed.ac.uk>
2021-03-01 21:48:50 +0000

    fixed issues

--- mitm/eve.py
+++ mitm/eve.py
@@ -32,6 +32,7 @@ dialog = Dialog('print')
 print('PRETENDING TO BE ALICE...')
 player1 = 'alice'
 alice_socket, alice_aes = setup(player1, BUFFER_DIR, BUFFER_FILE_NAME)
+
 print('SOCKETS FOR COMMUNICATION WITH ALICE ESTABLISHED!')
 
 # establish a socket pretending to be bob
@@ -45,57 +46,57 @@ print('SOCKETS FOR COMMUNICATION WITH BOB ESTABLISHED!')
 
 
 
-received_from_alice = receive_and_decrypt(bob_aes, bob_socket) # message from alice
+received_from_bob = receive_and_decrypt(alice_aes, alice_socket) # message from alice
 
 # If relay is the argument
 if args.relay:
 
-    encrypt_and_send(received_from_alice, alice_aes, alice_socket)
-    dialog.chat('Alice said: "{}"'.format(received_from_alice))
-
-    dialog.info('Message sent! Waiting for reply...')
-    received_from_bob = receive_and_decrypt(alice_aes, alice_socket)
-
+    encrypt_and_send(received_from_bob, bob_aes, bob_socket)
     dialog.chat('Bob said: "{}"'.format(received_from_bob))
 
-    encrypt_and_send(received_from_bob, bob_aes, bob_socket)
+    dialog.info('Message sent! Waiting for reply...')
+    received_from_alice = receive_and_decrypt(bob_aes, bob_socket)
+
+    dialog.chat('Alice said: "{}"'.format(received_from_Alice))
+
+    encrypt_and_send(received_from_alice, alice_aes, alice_socket)
 
 #if Alice is not in mood to continue meeting Bob
 elif args.break_heart:
     
     # Make alice send BAD_MSG instead of NICE_MSG to bob
-    to_send = BAD_MSG[player1] 
+    to_send = BAD_MSG[player2] 
 
-    encrypt_and_send(to_send, alice_aes, alice_socket)
-    dialog.chat('Alice said: "{}"'.format(to_send))
+    encrypt_and_send(to_send, bob_aes, bob_socket)
+    dialog.chat('Bob said: "{}"'.format(to_send))
 
     dialog.info('Message sent! Waiting for reply...')
-    received_from_bob = receive_and_decrypt(alice_aes, alice_socket)
+    received_from_alice = receive_and_decrypt(bob_aes, bob_socket)
 
-    dialog.chat('Bob said: "{}"'.format(received_from_bob))
+    dialog.chat('Bob said: "{}"'.format(received_from_alice))
 
-    encrypt_and_send(received_from_bob, bob_aes, bob_socket)
+    encrypt_and_send(received_from_alice, alice_aes, alice_socket)
 
 #Send custom message
 elif args.custom:
     
-    dialog.chat('Alice said: "{}"'.format(received_from_alice))
-
-    # Prompt the user for alice's message
-    dialog.prompt('Input what you would like Alice to say to Bob')
-    alice_send = input()
-
-    encrypt_and_send(alice_send, alice_aes, alice_socket)
-
-    dialog.info('Message sent! Waiting for reply...')
-    received_from_bob = receive_and_decrypt(alice_aes, alice_socket)
     dialog.chat('Bob said: "{}"'.format(received_from_bob))
 
-    # Prompt the user for bob's message
+    # Prompt the user for alice's message
     dialog.prompt('Input what you would like Bob to say to Alice')
     bob_send = input()
 
     encrypt_and_send(bob_send, bob_aes, bob_socket)
+
+    dialog.info('Message sent! Waiting for reply...')
+    received_from_alice = receive_and_decrypt(bob_aes, bob_socket)
+    dialog.chat('Alice said: "{}"'.format(received_from_alice))
+
+    # Prompt the user for bob's message
+    dialog.prompt('Input what you would like Alice to say to Bob')
+    alice_send = input()
+
+    encrypt_and_send(alice_send, alice_aes, alice_socket)
 
 tear_down(alice_socket, BUFFER_DIR,BUFFER_FILE_NAME)
 tear_down(bob_socket, BUFFER_DIR,"new_buffer" )
