b1571066741e8947089fd984cc9d7ee9a0e2fb07
orgesskura <s1813106@ed.ac.uk>
2021-03-01 21:30:31 +0000

    added different flags

--- mitm/eve.py
+++ mitm/eve.py
@@ -9,8 +9,8 @@ from const import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--relay", help="Relay message from Bob to Alice.", action="store_true")
-parser.add_argument("--break-heart", help="Alice is going to break Bob's heart.", action="store_true")
-parser.add_argument("--custom", help="Send custom message.", action="store_true")
+parser.add_argument("--break-heart", help="Alice is going to break Bob's heart and end the relationship.", action="store_true")
+parser.add_argument("--custom", help="Send custom message. Do not be very cruel", action="store_true")
 args = parser.parse_args()
 
 if len(sys.argv) != 2:
@@ -20,8 +20,82 @@ if len(sys.argv) != 2:
 if args.relay:
     print("EVE IS ON RELAY MODE. No attack incoming")
 elif args.break_heart:
-    print("EVE IS ON BREAK HEART MODE. No new couple anymore.")
+    print("EVE IS ON BREAK HEART MODE. Finally, a love story worse than Twilight.")
 elif args.custom:
     print("EVE IS ON CUSTOM MODE. You can hook them up or not.")
 
 print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+dialog = Dialog('print')
+
+
+# establish a socket pretending to be alice
+print('PRETENDING TO BE ALICE...')
+player1 = 'alice'
+alice_socket, alice_aes = setup(player1, BUFFER_DIR, BUFFER_FILE_NAME)
+print('SOCKETS FOR COMMUNICATION WITH ALICE ESTABLISHED!')
+
+# establish a socket pretending to be bob
+print('PRETENDING TO BE BOB...')
+player2 = 'bob'
+bob_socket, bob_aes = setup(player2, BUFFER_DIR, BUFFER_FILE_NAME)
+os.rename(BUFFER_DIR + BUFFER_FILE_NAME, BUFFER_DIR + "new_buffer")
+print('SOCKETS FOR COMMUNICATION WITH BOB ESTABLISHED!')
+
+
+
+
+
+received_from_alice = receive_and_decrypt(bob_aes, bob_socket) # message from alice
+
+# If relay is the argument
+if args.relay:
+
+    encrypt_and_send(received_from_alice, alice_aes, alice_socket)
+    dialog.chat('Alice said: "{}"'.format(received_from_alice))
+
+    dialog.info('Message sent! Waiting for reply...')
+    received_from_bob = receive_and_decrypt(alice_aes, alice_socket)
+
+    dialog.chat('Bob said: "{}"'.format(received_from_bob))
+
+    encrypt_and_send(received_from_bob, bob_aes, bob_socket)
+
+#if Alice is not in mood to continue meeting Bob
+elif args.break_heart:
+    
+    # Make alice send BAD_MSG instead of NICE_MSG to bob
+    to_send = BAD_MSG[player1] 
+
+    encrypt_and_send(to_send, alice_aes, alice_socket)
+    dialog.chat('Alice said: "{}"'.format(to_send))
+
+    dialog.info('Message sent! Waiting for reply...')
+    received_from_bob = receive_and_decrypt(alice_aes, alice_socket)
+
+    dialog.chat('Bob said: "{}"'.format(received_from_bob))
+
+    encrypt_and_send(received_from_bob, bob_aes, bob_socket)
+
+#Send custom message
+elif args.custom:
+    
+    dialog.chat('Alice said: "{}"'.format(received_from_alice))
+
+    # Prompt the user for alice's message
+    dialog.prompt('Input what you would like Alice to say to Bob')
+    alice_send = input()
+
+    encrypt_and_send(alice_send, alice_aes, alice_socket)
+
+    dialog.info('Message sent! Waiting for reply...')
+    received_from_bob = receive_and_decrypt(alice_aes, alice_socket)
+    dialog.chat('Bob said: "{}"'.format(received_from_bob))
+
+    # Prompt the user for bob's message
+    dialog.prompt('Input what you would like Bob to say to Alice')
+    bob_send = input()
+
+    encrypt_and_send(bob_send, bob_aes, bob_socket)
+
+tear_down(alice_socket, BUFFER_DIR,BUFFER_FILE_NAME)
+tear_down(bob_socket, BUFFER_DIR,"new_buffer" )
