d759c1c0a05cb25de3e37b9dd074becf9c4285fc
orgesskura <s1813106@ed.ac.uk>
2021-03-01 20:38:12 +0000

    added a few messages

--- mitm/eve.py
+++ mitm/eve.py
@@ -14,5 +14,14 @@ parser.add_argument("--custom", help="Send custom message.", action="store_true"
 args = parser.parse_args()
 
 if len(sys.argv) != 2:
-    print("You must pick exactly one of the following flags: --relay, --break-heart, --custom")
+    print("You must choose exactly one of the flags: --relay, --break-heart, --custom")
     sys.exit(1)
+
+if args.relay:
+    print("EVE IS ON RELAY MODE. No attack incoming")
+elif args.break_heart:
+    print("EVE IS ON BREAK HEART MODE. No new couple anymore.")
+elif args.custom:
+    print("EVE IS ON CUSTOM MODE. You can hook them up or not.")
+
+print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
